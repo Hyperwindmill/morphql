74df54c64d5a881ff9499529c06aa4243d7ca1d6
Hyperwindmill <48651572+Hyperwindmill@users.noreply.github.com>
2026-02-11 23:43:45 +0100

    version update

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -27,7 +27,7 @@ program
   .description(
     "CLI tool for morphql - transform structural data from the command line.",
   )
-  .version("0.1.16");
+  .version("0.1.17");
 
 // --- 1. Batch Subcommand ---
 program
--- packages/jetbrains-extension/build.gradle.kts
+++ packages/jetbrains-extension/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "org.morphql"
-version = "0.1.16"
+version = "0.1.17"
 
 repositories {
     mavenCentral()
