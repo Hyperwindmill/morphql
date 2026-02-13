69770fbead1f2e7b569ce1f247a2580691c9161a
Hyperwindmill <48651572+Hyperwindmill@users.noreply.github.com>
2026-02-13 01:19:11 +0100

    fix configuration precedence

--- packages/php-symfony/tests/DependencyInjection/ConfigurationTest.php
+++ packages/php-symfony/tests/DependencyInjection/ConfigurationTest.php
@@ -19,11 +19,11 @@ class ConfigurationTest extends TestCase
 
         $this->assertEquals('cli', $result['provider']);
         $this->assertNull($result['cli_path']);
-        $this->assertEquals('node', $result['node_path']);
+        $this->assertNull($result['node_path']);
         $this->assertEquals('%kernel.cache_dir%/morphql', $result['cache_dir']);
-        $this->assertEquals('http://localhost:3000', $result['server_url']);
+        $this->assertNull($result['server_url']);
         $this->assertNull($result['api_key']);
-        $this->assertEquals(30, $result['timeout']);
+        $this->assertNull($result['timeout']);
         $this->assertEquals('%kernel.project_dir%/morphql-queries', $result['query_dir']);
     }
 
@@ -31,12 +31,6 @@ class ConfigurationTest extends TestCase
     {
         $this->expectException(\Exception::class);
         $this->process(['provider' => 'ftp']);
-    }
-
-    public function testTimeoutMinimumValue(): void
-    {
-        $this->expectException(\Exception::class);
-        $this->process(['timeout' => 0]);
     }
 
     public function testCustomValues(): void
