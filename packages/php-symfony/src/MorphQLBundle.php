0aa4a0e3685ea84ec9dbd364201177d957553a31
Hyperwindmill <48651572+Hyperwindmill@users.noreply.github.com>
2026-02-14 23:39:57 +0100

    Update MorphQLBundle.php

--- packages/php-symfony/src/MorphQLBundle.php
+++ packages/php-symfony/src/MorphQLBundle.php
@@ -2,8 +2,18 @@
 
 namespace MorphQL\SymfonyBundle;
 
+use MorphQL\SymfonyBundle\DependencyInjection\MorphQLExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class MorphQLBundle extends Bundle
 {
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        if (null === $this->extension) {
+            $this->extension = new MorphQLExtension();
+        }
+
+        return $this->extension;
+    }
 }
