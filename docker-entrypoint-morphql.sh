a7e370edaa5109bcda73e719c6eb09bd9984ff9e
Hyperwindmill <48651572+Hyperwindmill@users.noreply.github.com>
2026-03-14 10:43:41 +0100

    reset docker strategy

--- docker-entrypoint-morphql.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# This script runs every time the container starts.
-# It ensures that our baked-in custom node is linked correctly inside the persist node volume
-mkdir -p /home/node/.n8n/custom/node_modules
-ln -sfn /custom-extensions/node_modules/n8n-nodes-morphql /home/node/.n8n/custom/node_modules/n8n-nodes-morphql
-
-# Execute the original n8n entrypoint
-exec /docker-entrypoint.sh "$@"
